38f5c548cb5240178a66a8ca70c2fe33be0edab5
RaulDiaz <rulhdiaz@gmail.com>
2024-05-13 14:47:15 -0600

    commit

--- thiio-app/resources/js/layouts/Login.vue
+++ thiio-app/resources/js/layouts/Login.vue
@@ -11,6 +11,7 @@
               <v-text-field v-model="email" label="Email" type="email" required></v-text-field>
               <v-text-field v-model="password" label="Password" type="password" required></v-text-field>
               <v-btn color="primary" type="submit">Login</v-btn>
+              <v-alert v-if="errorMessage" type="error" dismissible>{{ errorMessage }}</v-alert>
             </v-form>
           </v-card-text>
         </v-card>
@@ -26,25 +27,27 @@ export default {
   data() {
     return {
       email: '',
-      password: ''
+      password: '',
+      errorMessage: ''
     };
   },
   methods: {
     async login() {
-   try {
-     const response = await axios.post('http://127.0.0.1:8000/api/login', {
-       email: this.email,
-       password: this.password
-     });
-     console.log('Login successful:', response.data);
-     // Redirect user to dashboard or another page
-     console.log('Navigating to dashboard...');
-     this.$router.push('/dashboard');
-     console.log('Navigation complete');
-   } catch (error) {
-     console.error('Login error:', error.response.data);
-   }
- }
+      try {
+        const response = await axios.post('http://127.0.0.1:8000/api/login', {
+          email: this.email,
+          password: this.password
+        });
+        console.log('Login successful:', response.data);
+        // Redirect user to dashboard or another page
+        console.log('Navigating to dashboard...');
+        this.$router.push('/dashboard');
+        console.log('Navigation complete');
+      } catch (error) {
+        console.error('Login error:', error.response.data);
+        this.errorMessage = error.response.data.message; // Assuming the error message is provided in the response
+      }
+    }
   }
 };
 </script>
